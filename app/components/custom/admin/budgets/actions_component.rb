73ad9454534fd112e21d13db1fe516b540d1f33e
CoslaJohn <johnmunro@cosla.gov.uk>
2025-07-11 11:38:04 +0100

    add questions button to budget admin page

--- app/components/custom/admin/budgets/actions_component.rb
+++ app/components/custom/admin/budgets/actions_component.rb
@@ -41,64 +41,10 @@ class Admin::Budgets::ActionsComponent < ApplicationComponent
     end
     
     def questions_action
-      puts "TRYING TO DO QUESTONS"
-      render Admin::Budgets::QuestionsButtonComponent.new(budget)
-    end
-
-    def winners_action
-      render Admin::Budgets::CalculateWinnersButtonComponent.new(budget)
-    end
-
-    def winners_hint
-      t("admin.budgets.actions.descriptions.calculate_winners", phase: t("budgets.phase.finished"))
-    end
-
-    def destroy_action
-      action(:destroy,
-             text: t("admin.budgets.edit.delete"),
-             confirm: t("admin.budgets.actions.confirm.destroy"),
-             disabled: budget.investments.any? || budget.poll)
-    end
-
-    def destroy_hint
-      if budget.investments.any?
-        t("admin.budgets.destroy.unable_notice")
-      elsif budget.poll
-        t("admin.budgets.destroy.unable_notice_polls")
-      else
-        t("admin.budgets.actions.descriptions.destroy")
-      end
-    end
-
-    def ballots_action
-      if budget.published? && !budget.balloting_finished? && !budget.poll.present?
-        action(:ballots,
-               text: t("admin.budgets.actions.ballots"),
-               path: create_budget_poll_path,
-               method: :post,
-               confirm: t("admin.budgets.actions.confirm.ballots"),
-               disabled: !feature?("polls"))
-      end
-    end
-
-    def ballots_hint
-      if feature?("polls")
-        t("admin.budgets.actions.descriptions.ballots")
-      else
-        link = admin_settings_path(anchor: "tab-participation-processes")
-        t("admin.budgets.ballots.feature_disabled", link: link)
-      end
-    end
-
-    def create_budget_poll_path
-      balloting_phase = budget.phases.find_by(kind: "balloting")
-
-      admin_polls_path(poll: {
-        name: budget.name,
-        budget_id: budget.id,
-        starts_at: balloting_phase.starts_at,
-        ends_at: balloting_phase.ends_at
-      })
+      action(:questions,
+         path: admin_budget_budget_questions_path(budget),
+         text: t("admin.budgets.actions.questions", default: "Manage Questions"),
+         class: "button expanded")
     end
 
     def descriptor_id(action_name)
