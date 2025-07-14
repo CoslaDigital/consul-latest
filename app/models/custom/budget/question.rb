c13b4550c58cda034ba2a25c9df57c69ff9781ab
CoslaJohn <johnmunro@cosla.gov.uk>
2025-07-14 15:21:22 +0100

    fix(style): linters

--- app/models/custom/budget/question.rb
+++ app/models/custom/budget/question.rb
@@ -1,21 +1,19 @@
 class Budget
-    class Question < ApplicationRecord
-      translates :text, :hint,  touch: true
-      include Globalizable
-  
-      belongs_to :budget, touch: true
-      has_many :answers, class_name: "Investment::Answer"
+  class Question < ApplicationRecord
+    translates :text, :hint, touch: true
+    include Globalizable
 
-      validates_translation :text, presence: true
-    
-      scope :enabled, -> { where(enabled: true) }
-      
-      
-      def title
-        text
-      end
-      private
-  
+    belongs_to :budget, touch: true
+    has_many :answers, class_name: "Investment::Answer"
+
+    validates_translation :text, presence: true
+
+    scope :enabled, -> { where(enabled: true) }
+
+    def title
+      text
     end
+
+    private
   end
-  
+end
