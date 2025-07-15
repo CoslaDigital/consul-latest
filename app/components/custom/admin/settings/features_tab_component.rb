8f488b0fd33c7ca743c3d354af7ca17b02d9977d
CoslaJohn <johnmunro@cosla.gov.uk>
2025-07-15 09:39:06 +0100

    Revert "Merge branch 'beta' into djnd"
    This reverts commit a8b365472285cc92a5a46f80a34072ddb7811b66, reversing
    changes made to 14f3129e9ff931bb05a1a1cc2efd358ab9abc27d.

--- app/components/custom/admin/settings/features_tab_component.rb
+++ app/components/custom/admin/settings/features_tab_component.rb
@@ -10,7 +10,6 @@ class Admin::Settings::FeaturesTabComponent < ApplicationComponent
                           feature.hide_local_login
                           feature.hide_comments
                           feature.hide_votes
-                          feature.hide_dash_progress
                         ]
     original_settings + custom_settings
   end
