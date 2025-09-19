cd225ffba5817ca39b5550e5b9ddee6e5f983251
CoslaJohn <consul@cosla.gov.uk>
2025-09-19 11:32:03 +0100

    create new portrait image variant and associated style

--- app/models/custom/image.rb
+++ app/models/custom/image.rb
@@ -4,7 +4,7 @@ class Image < ApplicationRecord
 
   def self.styles
     {
-      
+
       hero_small:  { gravity: "center", resize: "768x", crop: "768x192+0+0" },
       hero_medium: { gravity: "center", resize: "1280x", crop: "1280x320+0+0" },
       hero_large:  { gravity: "center", resize: "1920x", crop: "1920x480+0+0" },
@@ -13,6 +13,8 @@ class Image < ApplicationRecord
       large: { resize: "x#{Setting["uploads.images.min_height"]}" },
       medium: { gravity: "center", resize: "300x300^", crop: "300x300+0+0" },
       thumb: { gravity: "center", resize: "140x245^", crop: "140x245+0+0" },
+      portrait: { gravity: "center", resize: "160x160^", crop: "160x160+0+0" }
+    thumb : { gravity: "center", resize: "140x245^", crop: "140x245+0+0" },
       feed_thumb: { gravity: "center", resize: "600x192^", crop: "600x192+0+0" }
     }
   end
