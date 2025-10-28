475e80bc501efa4515295a8446b0309651b84a18
CoslaJohn <johnmunro@cosla.gov.uk>
2025-10-28 14:04:58 +0000

    improve layout of my ballot component

---
+++ app/components/custom/budgets/ballot/investment_for_sidebar_component.rb
@@ -0,0 +1,28 @@
+class Budgets::Ballot::InvestmentForSidebarComponent < Budgets::Ballot::InvestmentComponent; end
+
+load Rails.root.join("app","components","budgets","ballot","investment_for_sidebar_component.rb")
+
+class Budgets::Ballot::InvestmentForSidebarComponent < Budgets::Ballot::InvestmentComponent
+  with_collection_parameter :investment
+  attr_reader :investment, :investment_ids, :preference_number
+
+  def initialize(investment:, investment_ids:,investment_iteration: nil)
+    super(investment: investment)
+    @investment_ids = investment_ids
+    @preference_number = investment_iteration&.index&.+(1)
+  end
+
+  private
+
+    def list_item_id
+      "#{super}_sidebar"
+    end
+
+    def investment_title
+      tag.span investment.title, class: "ballot-list-title"
+    end
+
+    def delete_path
+      budget_ballot_line_path(budget, id: investment.id, investments_ids: investment_ids)
+    end
+end
