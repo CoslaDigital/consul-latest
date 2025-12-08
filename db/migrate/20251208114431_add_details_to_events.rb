5b4c0a59a1e4689bb8a74d7aeb34b4e26958c66b
CoslaJohn <johnmunro@cosla.gov.uk>
2025-12-08 12:20:42 +0000

    make migration deal with if column exists

--- db/migrate/20251208114431_add_details_to_events.rb
+++ db/migrate/20251208114431_add_details_to_events.rb
@@ -1,6 +1,6 @@
 class AddDetailsToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_column :events, :location, :string
-    add_column :events, :event_type, :string
+    add_column :events, :location, :string, if_not_exists: true
+    add_column :events, :event_type, :string, if_not_exists: true
   end
 end
