a1ed8413aa49fe091cbc1c32524e80b48891a3db
coslajohn <johnmunro@cosla.gov.uk>
2025-12-08 11:58:43 +0000

    move event admin to admin menu

--- app/components/admin/menu_component.rb
+++ app/components/admin/menu_component.rb
@@ -28,6 +28,7 @@ class Admin::MenuComponent < ApplicationComponent
         profiles_links,
         stats_link,
         settings_links,
+        events_link,
         dashboard_links,
         (machine_learning_link if ::MachineLearning.enabled?)
       ]
@@ -279,8 +280,7 @@ class Admin::MenuComponent < ApplicationComponent
           information_texts_link,
           documents_link,
           images_link,
-          content_blocks_link,
-          events_link
+          content_blocks_link
         )
       end
     end
