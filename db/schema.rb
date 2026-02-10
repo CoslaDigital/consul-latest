7b3b86efbce00d3566e9aa79ac3d69ff16965376
CoslaJohn <consul@cosla.gov.uk>
2026-02-10 12:22:00 +0000

    Add sentiment analysis to Machine Learning comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_09_085528) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_10_105211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -971,6 +971,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_09_085528) do
     t.text "body"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.jsonb "sentiment_analysis"
   end
 
   create_table "moderators", id: :serial, force: :cascade do |t|
