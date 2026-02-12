301e24030ad783b08bba8083ac27e912d0daa47f
CoslaJohn <consul@cosla.gov.uk>
2026-02-12 14:05:19 +0000

    Add dry_run flag to MachineLearningJobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_09_085528) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_12_121131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -917,6 +917,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_09_085528) do
     t.bigint "user_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.boolean "dry_run"
     t.index ["user_id"], name: "index_machine_learning_jobs_on_user_id"
   end
 
