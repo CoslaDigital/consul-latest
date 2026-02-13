e7d5dfbceacf4bbea57bf31048af88adf90af76d
CoslaJohn <consul@cosla.gov.uk>
2026-02-13 15:43:08 +0000

    Add stats to machine learning jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_12_121131) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_13_150917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -918,6 +918,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_12_121131) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.boolean "dry_run"
+    t.integer "duration"
+    t.integer "total_tokens"
     t.index ["user_id"], name: "index_machine_learning_jobs_on_user_id"
   end
 
