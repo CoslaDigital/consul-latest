c6c131f64a8339de49bef07765b75f5952b25bbf
CoslaJohn <consul@cosla.gov.uk>
2026-02-16 14:20:55 +0000

    fix layout inconsistencies

--- app/components/budgets/investments/ballot_component.rb
+++ app/components/budgets/investments/ballot_component.rb
@@ -10,40 +10,40 @@ class Budgets::Investments::BallotComponent < ApplicationComponent
 
   private
 
-    def budget
-      ballot.budget
-    end
+  def budget
+    ballot.budget
+  end
 
-    def voted?
-      ballot.has_investment?(investment)
-    end
+  def voted?
+    ballot.has_investment?(investment)
+  end
 
-    def reason
-      @reason ||= investment.reason_for_not_being_ballotable_by(current_user, ballot)
-    end
+  def reason
+    @reason ||= investment.reason_for_not_being_ballotable_by(current_user, ballot)
+  end
 
-    def vote_aria_label
-      t("budgets.investments.investment.add_label", investment: investment.title)
-    end
+  def vote_aria_label
+    t("budgets.investments.investment.add_label", investment: investment.title)
+  end
 
-    def remove_vote_aria_label
-      t("budgets.ballots.show.remove_label", investment: investment.title)
-    end
+  def remove_vote_aria_label
+    t("budgets.ballots.show.remove_label", investment: investment.title)
+  end
 
-    def link_to_my_heading
-      link_to(investment.heading.name,
-              budget_investments_path(budget_id: investment.budget_id,
-                                      heading_id: investment.heading_id))
-    end
+  def link_to_my_heading
+    link_to(investment.heading.name,
+            budget_investments_path(budget_id: investment.budget_id,
+                                    heading_id: investment.heading_id))
+  end
 
-    def link_to_change_ballot
-      link_to(t("budgets.ballots.reasons_for_not_balloting.change_ballot"),
-              budget_ballot_path(budget))
-    end
+  def link_to_change_ballot
+    link_to(t("budgets.ballots.reasons_for_not_balloting.change_ballot"),
+            budget_ballot_path(budget))
+  end
 
-    def assigned_heading
-      ballot.heading_for_group(investment.group)
-    end
+  def assigned_heading
+    ballot.heading_for_group(investment.group)
+  end
 
   def cannot_vote_text
     # Return early if there's no reason to show a message
