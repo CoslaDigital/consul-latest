consuldemocracy/consuldemocracy

Use a <time> tag to display dates in moderation tables
Just like we did in commits 57675d40f and 040438fe0.

app/components/moderation/shared/table_component.rb
@@ -49,4 +49,8 @@ class Moderation::Shared::TableComponent < ApplicationComponent
         { "aria-label": record.title }
       end
     end
+
+    def updated_at_date(record)
+      time_tag record.created_at.to_date, format: :default, class: "date"
+    end
 end

CoslaDigital/consul-latest

app/components/moderation/shared/table_component.rb
@@ -48,4 +48,8 @@ class Moderation::Shared::TableComponent < ApplicationComponent
     def check_box_aria_attributes(record)
       { "aria-label": record.human_name }
     end
+
+    def updated_at_date(record)
+      time_tag record.updated_at.to_date, format: :default, class: "date"
+    end
 end