6169f4a910d64c40ec4f9fb1274e1f3ea560e104
CoslaJohn <consul@cosla.gov.uk>
2026-02-25 13:51:19 +0000

    updated rspec tests

--- spec/factories/machine_learning.rb
+++ spec/factories/machine_learning.rb
@@ -5,6 +5,27 @@ FactoryBot.define do
     started_at { Time.current }
     dry_run { false }
     config { {} }
+
+    trait :active do
+      started_at { Time.current }
+      finished_at { nil }
+      error { nil }
+    end
+
+    trait :finished do
+      finished_at { Time.current }
+    end
+
+    trait :errored do
+      error { "StandardError: Something went wrong" }
+    end
+  end
+
+  factory :machine_learning_info do
+    kind { "comments_summary" }
+    script { "proposal_tags" }
+    generated_at { Time.current }
+    updated_at { Time.current }
   end
 
   factory :ml_summary_comment do
