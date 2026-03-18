25668e48c34a441bfca685169fb7f3c8e3dfd704
Rory Gianni <hello@rgianni.cc>
2026-03-18 15:29:08 +0000

    fall back to using default_links in standard component so we get sensemakers

--- app/components/custom/admin/menu_component.rb
+++ app/components/custom/admin/menu_component.rb
@@ -19,28 +19,6 @@ class Admin::MenuComponent < ApplicationComponent
 
   private
 
-    def default_links
-      [
-        (proposals_link if feature?(:proposals)),
-        (debates_link if feature?(:debates)),
-        comments_link,
-        (polls_link if feature?(:polls)),
-        (legislation_link if feature?(:legislation)),
-        (budgets_link if feature?(:budgets)),
-        booths_links,
-        (signature_sheets_link if feature?(:signature_sheets)),
-        messages_links,
-        site_customization_links,
-        moderated_content_links,
-        profiles_links,
-        stats_link,
-       (events_link if feature?(:events)),
-        settings_links,
-        dashboard_links,
-        (machine_learning_link if ::MachineLearning.enabled?)
-      ]
-    end
-
   def process_manager_links
     [
       (proposals_link if feature?(:proposals)),
