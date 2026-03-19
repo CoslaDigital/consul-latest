a19d0fcead4a908bcdb1ebe348a22b1e568522da
CoslaJohn <consul@cosla.gov.uk>
2026-03-19 12:51:03 +0000

    partially fix custom proposal and comments views to use refactored comments components. Updated comments to allow masking of usernames from cosla feature

--- app/components/comments/info_component.rb
+++ app/components/comments/info_component.rb
@@ -1,8 +1,13 @@
 class Comments::InfoComponent < ApplicationComponent
-  attr_reader :comment, :valuation
+  attr_reader :comment, :valuation, :current_user
 
-  def initialize(comment, valuation: false)
+  def initialize(comment, valuation: false, current_user: nil)
     @comment = comment
     @valuation = valuation
+    @current_user = current_user
+  end
+
+  def render?
+    comment.present?
   end
 end
