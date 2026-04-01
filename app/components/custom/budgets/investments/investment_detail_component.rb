fc5e57733e08152454d5ecd776d5d9a626394a70
CoslaJohn <consul@cosla.gov.uk>
2026-04-01 13:30:13 +0100

    fix view to component issues in custom investment detail

--- app/components/custom/budgets/investments/investment_detail_component.rb
+++ app/components/custom/budgets/investments/investment_detail_component.rb
@@ -6,7 +6,7 @@ class Budgets::Investments::InvestmentDetailComponent < ApplicationComponent
   attr_reader :investment, :preview
   alias_method :preview?, :preview
   delegate :auto_link_already_sanitized_html, :map_location_available?,
-           :render_image, :render_map, :wysiwyg, to: :helpers
+           :render_image, :render_map, :wysiwyg, :sanitize_and_auto_link, to: :helpers
 
   def initialize(investment, preview: false)
     @investment = investment
