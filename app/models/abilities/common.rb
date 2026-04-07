1dfaefcde9043e97e9a188c3572a63ade984abe4
CoslaJohn <johnmunro@cosla.gov.uk>
2026-04-07 16:03:42 +0100

    milestone changes - may or may not work

--- app/models/abilities/common.rb
+++ app/models/abilities/common.rb
@@ -87,9 +87,9 @@ module Abilities
 
       can [:budget_headings, :select, :select_headings], Budget
 
+
       if user.level_two_or_three_verified?
         can :vote, Proposal, &:published?
-
         can [:create, :destroy], ActsAsVotable::Vote, voter_id: user.id, votable_type: "Legislation::Proposal"
 
         can :create, Legislation::Answer
@@ -117,6 +117,40 @@ module Abilities
         end
         can :destroy, Poll::Answer do |answer|
           answer.author == user && answer.question.answerable_by?(user)
+        end
+        
+        # A user can manage a milestone if its parent ('milestoneable')
+        # has a user_id that matches their own.
+        # The :edit permission is automatically granted if the user can :update.
+        can [:update, :destroy], Milestone do |milestone|
+  # --- Start of Debugging Log ---
+  puts "\n" # Adds a space in the log to make it easy to find
+  puts "--- Checking Milestone Permissions for User ##{user.id} ---"
+  puts "Milestone ID: #{milestone.id}"
+
+  if milestone.milestoneable.present?
+    puts "Parent Object: #{milestone.milestoneable.class.name} ##{milestone.milestoneable.id}"
+    puts "Parent Author ID: #{milestone.milestoneable.author_id}"
+  else
+    puts "Parent Object: nil"
+  end
+
+  puts "Current User ID: #{user.id}"
+
+  # This variable will hold the final decision
+  decision = milestone.milestoneable&.author_id == user.id
+
+  puts "Decision (IDs Match?): #{decision}"
+  puts "--- End of Check ---\n"
+  # --- End of Debugging Log ---
+
+  decision # The rule returns the final decision
+end        
+
+        # 2. Permission to CREATE milestones for their own investments
+        # A user can create a milestone if they are the author of the parent investment.
+        can :create, Milestone do |milestone|
+          milestone.milestoneable.author_id == user.id
         end
       end
 
