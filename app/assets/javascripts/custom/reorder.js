a544cd90225f76914ecf56d47bad6b32069cef77
CoslaJohn <consul@cosla.gov.uk>
2026-06-04 15:44:17 +0100

    fix draggable sidebar not responding

--- app/assets/javascripts/custom/reorder.js
+++ app/assets/javascripts/custom/reorder.js
@@ -1,10 +1,10 @@
 // This function will initialize the sortable lists
 var initializeSortable = function() {
-  
+
   // Find all lists with our data-controller="reorder" attribute
   $('ul[data-controller="reorder"]').each(function() {
     var $list = $(this);
-    
+
     // Check if it's already been initialized
     if ($list.data('sortable-initialized')) {
       return;
@@ -16,13 +16,13 @@ var initializeSortable = function() {
     $list.sortable({
       // 1. The element to click and drag
       handle: '.ballot-item__handle, .drag-handle',
-      
+
       // 2. The items inside the list to be sorted
       items: 'li[data-reorder-target="item"]',
-      
+
       // 3. A class for the placeholder element
       placeholder: 'sortable-placeholder',
-      
+
       // 4. This function is called when the user drops an item
       update: function(event, ui) {
         // Get the new order of IDs from the 'data-id' attribute
@@ -63,7 +63,7 @@ var initializeSortable = function() {
     $list.find('li[data-reorder-target="item"]').each(function(index) {
       var $item = $(this);
       var $prefElement = $item.find('.preference-order');
-      
+
       if ($prefElement.length > 0) {
         // Read the translation ("Preference") from the data-attribute
         var prefText = $prefElement.data('pref-text') || 'Preference';
@@ -79,3 +79,5 @@ $(document).ready(initializeSortable);
 
 // This ensures it also runs if you use Turbolinks
 $(document).on('turbolinks:load', initializeSortable);
+
+$(document).on('ajaxSuccess', initializeSortable);
