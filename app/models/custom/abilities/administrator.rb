b93817597d12aa9ca57cf5dc7f26d51bef5dd6fd
CoslaJohn <consul@cosla.gov.uk>
2026-07-16 15:06:04 +0100

    Allow admin to edit proposals

--- app/models/custom/abilities/administrator.rb
+++ app/models/custom/abilities/administrator.rb
@@ -21,6 +21,8 @@ module Abilities
       can :restore, Proposal
       cannot :restore, Proposal, hidden_at: nil
 
+      can :update, Proposal
+
       can :create, Legislation::Proposal
       can :show, Legislation::Proposal
       can :proposals, ::Legislation::Process
