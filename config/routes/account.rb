d0664242765f5c1dc9afadc4badebabd01c134f1
CoslaJohn <consul@cosla.gov.uk>
2026-07-22 15:08:22 +0100

    amend routes to use rails 8 syntax

--- config/routes/account.rb
+++ config/routes/account.rb
@@ -1,4 +1,5 @@
-resource :account, controller: "account", only: [:show, :update]
+resource :account, controller: "account", only: [:show, :update, :destroy] do
+  get :erase, on: :collection
   resource :two_factor_authentication, only: [:show, :create, :destroy] do
     post :enable, on: :member
     get :backup_codes, on: :collection
