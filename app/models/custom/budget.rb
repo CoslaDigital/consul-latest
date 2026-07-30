886e76cf3ab3922c05ed250cd42672a583cea249
CoslaJohn <consul@cosla.gov.uk>
2026-07-30 14:21:41 +0100

    update enum to new positional format

--- app/models/custom/budget.rb
+++ app/models/custom/budget.rb
@@ -6,7 +6,7 @@ class Budget < ApplicationRecord
   belongs_to :author, class_name: "User", optional: true
   has_many :questions, class_name: "Budget::Question"
 
-  enum kind: { budget: "budget", election: "election" }
+  enum :kind, { budget: "budget", election: "election" }
 
   before_save :enforce_election_settings, if: :election?
 
