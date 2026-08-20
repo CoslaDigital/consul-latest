ed28d426967cf3064748ca33bb355316f9ce1c54
CoslaJohn <consul@cosla.gov.uk>
2026-08-20 14:32:26 +0100

    fix missing routes after merge

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -176,7 +176,7 @@ namespace :admin do
       resources :managers, only: [:index, :create, :destroy]
     end
 
-    resources :users, only: :index do
+    resources :users, only: [:index, :new, :create, :edit, :update] do
       member do
         put :lock
         put :unlock
