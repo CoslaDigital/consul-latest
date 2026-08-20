098a38b978c5e31626009c06f7c4eac4620ed803
CoslaJohn <consul@cosla.gov.uk>
2026-08-20 16:35:38 +0100

    Add history to bulk user actions

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -190,8 +190,8 @@ namespace :admin do
     end
 
     namespace :users do
-      resources :generations, only: [:new, :create, :show]
-      resources :bulk_password_resets, only: [:show]
+      resources :generations, only: [:index, :new, :create, :show]
+      resources :bulk_password_resets, only: [:index, :show]
     end
 
     scope module: :poll do
